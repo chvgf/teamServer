6f268970e0ee8df3f0e37529012d94fa4c89c419
chvgf <chvgf@naver.com>
2024-02-20 00:47:02 +0900

    랜덤매칭

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const dotenv = require('dotenv');
-const path = require('path');
-const cors = require('cors');
-const paasport = require('passport');
-
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const path = require("path");
+const cors = require("cors");
+const paasport = require("passport");
 
 dotenv.config();
-const matchingRouter = require('./routes/matching');
-const communityRouter = require('./routes/community')
-const userRouter = require('./routes/user');
-const myCalendarRouter = require('./routes/myCalendar');
-const myPageRouter = require('./routes/myPage');
-const { connect } = require('./database/index');
-const passportConfig = require('./passport');
-
-
+const matchingRouter = require("./routes/matching");
+const communityRouter = require("./routes/community");
+const userRouter = require("./routes/user");
+const myCalendarRouter = require("./routes/myCalendar");
+const myPageRouter = require("./routes/myPage");
+const { connect } = require("./database/index");
+const passportConfig = require("./passport");
 
 // 라우터 가져오기
-const registerRouter = require('./routes/register')
-const clubRouter = require('./routes/club')
-const clubInsertRouter = require('./routes/clubInsert')
-
-
+const registerRouter = require("./routes/register");
+const clubRouter = require("./routes/club");
+const clubInsertRouter = require("./routes/clubInsert");
 
 const app = express();
-app.set('port', process.env.PORT || 3002);
+app.set("port", process.env.PORT || 3002);
 passportConfig();
 connect();
 
-app.use(cors({
-  origin: 'https://minton1000.netlify.app',
-  credentials: true
-}));
-app.use(morgan('dev'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: ["https://minton1000.netlify.app", "http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(express.json())
-app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-  name: 'session-cookie'
-}));
+app.use(express.json());
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
 app.use(paasport.initialize());
 app.use(paasport.session());
 
@@ -60,17 +59,14 @@ app.use((req, res, next) => {
 });
 
 // 라우터를 미들웨어로 등록
-app.use('/', matchingRouter);
-app.use('/community', communityRouter);
-app.use('/user', userRouter);
-app.use('/myCalendar', myCalendarRouter);
-app.use('/register', registerRouter);
-app.use('/club', clubRouter);
-app.use('/myPage', myPageRouter);
-app.use('/clubInsert', clubInsertRouter)
-
-
-
+app.use("/", matchingRouter);
+app.use("/community", communityRouter);
+app.use("/user", userRouter);
+app.use("/myCalendar", myCalendarRouter);
+app.use("/register", registerRouter);
+app.use("/club", clubRouter);
+app.use("/myPage", myPageRouter);
+app.use("/clubInsert", clubInsertRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -79,13 +75,13 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.error(err)
+  console.error(err);
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port') + '번에서 서버 실행 중입니다.');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port") + "번에서 서버 실행 중입니다.");
 });
--- routes/matching.js
+++ routes/matching.js
@@ -1,108 +1,130 @@
-const express = require('express');
-const { ObjectId } = require('mongodb');
+const express = require("express");
+const { ObjectId } = require("mongodb");
 
-const { client } = require('../database/index');
-const db = client.db('minton');
+const { client } = require("../database/index");
+const db = client.db("minton");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-    const matchingData = await db.collection('matching').find({}).toArray();
+    const matchingData = await db.collection("matching").find({}).toArray();
     res.json({
       flag: true,
-      message: '성공적으로 데이터를 가져왔습니다.',
+      message: "성공적으로 데이터를 가져왔습니다.",
       data: matchingData,
     });
   } catch (err) {
     res.json({
       flag: false,
-      message: '데이터 불러오기 실패',
+      message: "데이터 불러오기 실패",
     });
   }
 });
 
-router.post('/matchingInsert', async (req, res) => {
+router.post("/matchingInsert", async (req, res) => {
   const result = req.body;
-  const resultId = await db.collection('matching').insertOne({ ...result, joinMember: [req.user.userId] });
-  await db.collection('myMatchList').insertOne({
+  const resultId = await db.collection("matching").insertOne({ ...result, joinMember: [req.user.userId] });
+  await db.collection("myMatchList").insertOne({
     ...result,
     user: req.user._id,
     joinMember: [req.user.userId],
-    postId: resultId.insertedId
+    postId: resultId.insertedId,
   });
-  await db.collection('myCalendar').insertOne({
+  await db.collection("myCalendar").insertOne({
     title: req.body.title,
     start: req.body.selectDate,
     user: req.user._id,
-    postId: resultId.insertedId
+    postId: resultId.insertedId,
   });
-  res.send('저장성공')  
+  res.send("저장성공");
 });
 
-router.get('/matchingPost/:id', async (req, res) => {
-  const result = await db.collection('matching').findOne({ _id: new ObjectId(req.params.id) });
+router.get("/matchingPost/:id", async (req, res) => {
+  const result = await db.collection("matching").findOne({ _id: new ObjectId(req.params.id) });
   console.log(result);
   res.json({
     flag: true,
-    message: '상세페이지 불러오기 성공',
-    data: result
-  })
+    message: "상세페이지 불러오기 성공",
+    data: result,
+  });
 });
 
-router.delete('/deleteMatching/:id', async (req, res) => {
+router.delete("/deleteMatching/:id", async (req, res) => {
   try {
-    await db.collection('matching').deleteOne({ _id: new ObjectId(req.params.id) });
-    await db.collection('myCalendar').deleteMany({ postId: new ObjectId(req.params.id) })
-    await db.collection('myMatchList').deleteMany({ postId: new ObjectId(req.params.id) })
+    await db.collection("matching").deleteOne({ _id: new ObjectId(req.params.id) });
+    await db.collection("myCalendar").deleteMany({ postId: new ObjectId(req.params.id) });
+    await db.collection("myMatchList").deleteMany({ postId: new ObjectId(req.params.id) });
     res.json({
       flag: true,
-      message: '삭제 성공'
+      message: "삭제 성공",
     });
   } catch (err) {
     res.json({
       flag: false,
-      message: '삭제 실패'
+      message: "삭제 실패",
     });
   }
 });
 
-router.patch('/editMatchPost/:id', async (req, res) => {
+router.patch("/editMatchPost/:id", async (req, res) => {
   console.log(req.body);
   const { title, content, selectDate, district, joinPersonnel, game, gender } = req.body;
   try {
-    await db.collection('matching').updateOne({
-      _id: new ObjectId(req.params.id)
-    }, { $set: {
-      title,
-      content,
-      selectDate,
-      district,
-      joinPersonnel,
-      game,
-      gender
-    }
-  });
-    await db.collection('myMatchList').updateOne({
-      user: req.user._id, postId: new ObjectId(req.params.id)
-    }, { $set: {
-      title,
-      content,
-      selectDate,
-      district,
-      joinPersonnel,
-      game,
-      gender
-    }
-  });
-  await db.collection('myCalendar').updateOne({ user: new ObjectId(req.user._id) }, { $set: { title, start: selectDate } });
+    await db.collection("matching").updateOne(
+      {
+        _id: new ObjectId(req.params.id),
+      },
+      {
+        $set: {
+          title,
+          content,
+          selectDate,
+          district,
+          joinPersonnel,
+          game,
+          gender,
+        },
+      }
+    );
+    await db.collection("myMatchList").updateOne(
+      {
+        user: req.user._id,
+        postId: new ObjectId(req.params.id),
+      },
+      {
+        $set: {
+          title,
+          content,
+          selectDate,
+          district,
+          joinPersonnel,
+          game,
+          gender,
+        },
+      }
+    );
+    await db.collection("myCalendar").updateOne({ user: new ObjectId(req.user._id) }, { $set: { title, start: selectDate } });
     res.json({
       flag: true,
-      message: '수정 완료'
+      message: "수정 완료",
     });
   } catch (err) {
     console.error(err);
   }
+});
+
+// 랜덤매칭
+router.post("/RandomMatching", async (req, res) => {
+  try {
+    // console.log(req.user);
+  } catch (error) {
+    console.error(error);
+  }
+  // const userGrade = req.user.grade;
+  // const rnadomFilter = await db.collection("matching").findOne({ "id.grade": userGrade });
+  // console.log(rnadomFilter);
+  // res.send("저장성공");
 });
 
 module.exports = router;
